5f9efe27c0fc9342f5cf2fddefc29beabaa33b74
the-inhuman-account <31903083+the-inhuman-account@users.noreply.github.com>
2019-02-07 07:59:16 -0800

    Add files via upload

---
+++ RoboRIOThingy.java
@@ -0,0 +1,27 @@
+// https://wpilib.screenstepslive.com/s/currentCS/m/vision/l/669166-using-the-cameraserver-on-the-roborio
+
+package frc.robot;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+
+public class RoboRIOThingy {
+
+	private UsbCamera camera;
+	private CvSink sink;
+	private Mat source;
+
+	public void initRoboRIOCameraAlignmentSystem() {
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+		camera.setResolution(840,680);
+		CvSink cvSink = CameraServer.getInstance.getVideo();
+		Mat source = new Mat();
+				
+	}
+
+	public double roboRIOCameraAlignmentSystemGetAngleToTurn() {
+		cvSink.grabFrame(source);
+		MyVector choice = ProcessImage.processImage(source);
+	}
+}
