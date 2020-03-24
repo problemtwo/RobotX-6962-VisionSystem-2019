2911db29ffb802880abf33bfa32ee72ed3299eb7
AlphaBetaTester <abtesterofficial@gmail.com>
2020-03-23 19:43:35 -0700

    added Makefile

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# opencv_thingy_2
+# RobotX-6962-VisionSystem-2019
+
+This repository houses the code for FRC team 6962's computer vision system for the 2019 Destination: Deep Space game.
--- RoboRIOClient.java
+++ RoboRIOClient.java
@@ -6,7 +6,7 @@ import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 
-public class RoboRIOThingy {
+public class RoboRIOClient {
 
 	private UsbCamera camera;
 	private CvSink sink;
